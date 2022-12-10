6c7866f04810a9364254ece691892b4f5d993b60
Eitan Seri-Levi <eserilev@ucsc.edu>
2022-12-10 17:27:03 +0200

    update

--- client.py
+++ client.py
@@ -32,7 +32,7 @@ class VsockStream:
 
 def client_handler(args):
     client = VsockStream()
-    endpoint = (3, 5005)
+    endpoint = ("i-093d90abd3f89c8ec-enc184fca2de3734aa", 5005)
     client.connect(endpoint)
     unsigned_tx = ''
     client.send_data(unsigned_tx.encode())
