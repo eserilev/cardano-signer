83599cf68c52a51b7a109ccd4a3a8b69b044de47
Eitan Seri-Levi <eserilev@ucsc.edu>
2022-12-13 10:03:17 +0200

    update

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 docker build -t vsock-server -f Dockerfile.server .
-nitro-cli build-enclave --docker-uri vsock-server --output-file vsock_server.eif
-sudo nitro-cli run-enclave --eif-path vsock_server.eif --cpu-count 2 --memory 2054 --debug-mode
+nitro-cli build-enclave --docker-uri vsock-server --output-file vsock_sample.eif
+sudo nitro-cli run-enclave --eif-path vsock_sample.eif --cpu-count 2 --memory 2054 --debug-mode
